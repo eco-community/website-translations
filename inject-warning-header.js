307c519f52630b00974e201d0b80c49b68fe2ac1
Saulo <22698083+saulodigital@users.noreply.github.com>
2021-03-15 22:56:32 +0000

    Update inject-warning-header.js

--- inject-warning-header.js
+++ inject-warning-header.js
@@ -12,8 +12,8 @@ warningContainer.innerHTML = `
             width: 100%;
             z-index: 50000;
 ">
-    <p>This is an <b>unofficial</b> and community-driven proposal for the <a href="https://eco.com">Eco.com</a> website. This translation can be misleading or incorrect.<br>
-        If you find misleading, incorrect, or missing content and you want to contribute, please check: https://github.com/eco-translations/eco-translations.github.io
+    <p>This is an <b>unofficial</b> and community-driven proposal translation for the website <a href="https://eco.com">Eco.com</a>. This translation can be misleading or incorrect.<br>
+        If you find misleading, incorrect, or missing content and you want to contribute, please visit: https://eco-translations.github.io
     </p>
     <p>For the <b>official</b> content in English, please <b>visit the official website at <a href="https://eco.com">Eco.com</a> </b> </p>
 </div>
