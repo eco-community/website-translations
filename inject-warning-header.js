dfdae39fafae6f10db4ed00843bf92d789f08ca0
Jeremie Olivier <jeremie.olivier.pro@gmail.com>
2021-03-24 03:55:06 +0100

    fix header positionning + improve text + add multilanguage capability

--- inject-warning-header.js
+++ inject-warning-header.js
@@ -1,21 +1,72 @@
-let body = document.querySelector('body');
+path = window.location.href
+isInSupport = (path.search('support') + path.search('collection') + path.search('articles') ) > 0
+isInFr = path.search('/fr/') != -1
+isInRu = path.search('/ru/') != -1
+isInKo = path.search('/ko/') != -1
+
+let content; 
+
+if ( isInSupport)
+    content = document.querySelector('body');
+else 
+    content = document.querySelector('.page-wrapper');
+
+
+
+
+let enText = `
+<p>This is an <b>unofficial</b> and <b>community-driven</b> translation can be misleading or incorrect. For the <b>official</b> content in English, please visit <a href="https://eco.com">Eco.com</a><br>
+</p>
+<p>If you find misleading, incorrect, or missing content and you want to contribute, please visit <a href="https://github.com/eco-community/website-translations">The Eco community github</a>
+</p>
+`;
+
+let koText = `
+<p>koo This is an <b>unofficial</b> and <b>community-driven</b> translation can be misleading or incorrect. For the <b>official</b> content in English, please visit <a href="https://eco.com">Eco.com</a><br>
+</p>
+<p>If you find misleading, incorrect, or missing content and you want to contribute, please visit <a href="https://github.com/eco-community/website-translations">The Eco community github</a>
+</p>
+`;
+let ruText = `
+<p> ru This is an <b>unofficial</b> and <b>community-driven</b> translation can be misleading or incorrect. For the <b>official</b> content in English, please visit <a href="https://eco.com">Eco.com</a><br>
+</p>
+<p>If you find misleading, incorrect, or missing content and you want to contribute, please visit <a href="https://github.com/eco-community/website-translations">The Eco community github</a>
+</p>
+`;
+
+let frText = `
+<p> Cette traduction <b> non-officielle</b> réalisée par la communauté peut être incorrecte. Pour avoir accés au site <b>officiel</b> en Anglais, visitez <a href="https://eco.com">Eco.com</a><br>
+</p>
+<p>Si vous trouvez de mauvaises traductions,ou contenue manquant et que vous voulez contribuer, vous pouvez visiter  <a href="https://github.com/eco-community/website-translations">The Eco community github</a>
+</p>
+`;
+
+let headerText;
+
+if ( isInFr) headerText = frText;
+else if ( isInRu) headerText = ruText;
+else if ( isInKo) headerText = koText;
+else headerText = enText;
 
 
 let warningContainer = document.createElement('div');
 warningContainer.innerHTML = `  
-<div style="
+<div id="disclaimer-header" style="
             background-color: red;
             color: white;
             text-align: center;
             font-size: 16px;
             padding: 25px;
             width: 100%;
-            z-index: 50000;
+            z-index: 99999;
+            width: 100%;
 ">
-    <p>This is an <b>unofficial</b> and community-driven proposed translation for the website <a href="https://eco.com">Eco.com</a>. This translation can be misleading or incorrect.<br>
-        If you find misleading, incorrect, or missing content and you want to contribute, please visit: https://github.com/eco-community/website-translations
-    </p>
-    <p>For the <b>official</b> content in English, please <b>visit the official website at <a href="https://eco.com">Eco.com</a> </b> </p>
+${headerText}
+    
 </div>
 `;
-body.insertBefore(warningContainer, body.firstChild);
+content.insertBefore(warningContainer, content.firstChild);
+
+if ( ! isInSupport)
+content.style.paddingTop = '116px'; 
+
